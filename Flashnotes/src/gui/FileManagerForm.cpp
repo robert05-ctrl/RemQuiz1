3f1184e03d6419ccac0b6014506db7627904668e
robert05-ctrl <127037193+robert05-ctrl@users.noreply.github.com>
2025-06-09 12:08:50 +0900

    Use Unicode dash in MainWindow title

---
+++ Flashnotes/src/gui/FileManagerForm.cpp
@@ -0,0 +1,38 @@
+#include "FileManagerForm.h"
+#using <System.dll>
+#using <System.Windows.Forms.dll>
+#using <System.Drawing.dll>
+#include <msclr/marshal_cppstd.h>
+
+namespace FlashnotesGUI {
+
+FileManagerForm::FileManagerForm(flashnotes::FileController* ctrl)
+{
+    controller = ctrl;
+    Dock = DockStyle::Fill;
+
+    fileList = gcnew ListView();
+    fileList->Dock = DockStyle::Fill;
+
+    btnAdd = gcnew Button();
+    btnAdd->Text = "Add";
+    btnAdd->Dock = DockStyle::Bottom;
+    btnAdd->Click += gcnew EventHandler(this, &FileManagerForm::onAdd);
+
+    Controls->Add(fileList);
+    Controls->Add(btnAdd);
+}
+
+void FileManagerForm::onAdd(Object^ sender, EventArgs^ e)
+{
+    OpenFileDialog^ dlg = gcnew OpenFileDialog();
+    if (dlg->ShowDialog() == DialogResult::OK) {
+        auto res = controller->createFile(); // placeholder
+        if (!res)
+            MessageBox::Show(gcnew String(res.error().c_str()));
+        else
+            fileList->Items->Add(gcnew ListViewItem(gcnew String(std::to_string(res.value().id).c_str())));
+    }
+}
+
+} // namespace FlashnotesGUI
---
+++ Flashnotes/src/gui/NoteEditorForm.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#using <System.dll>
+#using <System.Windows.Forms.dll>
+#using <System.Drawing.dll>
+
+#include <controllers/NotesController.hpp>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+namespace FlashnotesGUI {
+
+public ref class NoteEditorForm : public UserControl
+{
+public:
+    NoteEditorForm(flashnotes::NotesController* ctrl);
+
+private:
+    flashnotes::NotesController* controller;
+    TextBox^ noteTitle;
+    TextBox^ noteBody;
+    Button^ btnSave;
+
+    void onSave(Object^ sender, EventArgs^ e);
+};
+
+} // namespace FlashnotesGUI
