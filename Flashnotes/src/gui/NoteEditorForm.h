c9144cd2af127ecb8f731efde84407983c44e790
robert05-ctrl <127037193+robert05-ctrl@users.noreply.github.com>
2025-06-09 11:03:24 +0900

    Add note listing and open support in GUI

--- Flashnotes/src/gui/NoteEditorForm.h
+++ Flashnotes/src/gui/NoteEditorForm.h
@@ -6,10 +6,9 @@
 
 #include <controllers/NotesController.hpp>
 
+namespace FlashnotesGUI {
 using namespace System;
 using namespace System::Windows::Forms;
-
-namespace FlashnotesGUI {
 
 public ref class NoteEditorForm : public UserControl
 {
@@ -18,10 +17,15 @@ public:
 
 private:
     flashnotes::NotesController* controller;
+    ListBox^ noteList;
     TextBox^ noteTitle;
     TextBox^ noteBody;
     Button^ btnSave;
+    Button^ btnOpen;
 
+    void loadNotes();
+    void onSelect(Object^ sender, EventArgs^ e);
+    void onOpen(Object^ sender, EventArgs^ e);
     void onSave(Object^ sender, EventArgs^ e);
 };
 
